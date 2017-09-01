b001f787c14ca9b9454a4796597202142ebfadb7
c4605 <bolasblack@gmail.com>
2017-09-02 00:00:12 +0800

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 var webpack = require('webpack');
+var path = require('path');
 var config = {};
 
 function generateConfig(name) {
   var uglify = name.indexOf('min') > -1;
   var config = {
     entry: './index.js',
+    module: {
+      rules: [
+        {
+          test: /\.json$/,
+          use: ['json-loader'],
+        },
+      ]
+    },
+    target: 'node',
     output: {
-      path: 'dist/',
+      path: path.resolve(__dirname, "dist"),
       filename: name + '.js',
       sourceMapFilename: name + '.map',
       library: 'axios',
-      libraryTarget: 'umd'
-    },
-    node: {
-      process: false
+      libraryTarget: 'commonjs2'
     },
     devtool: 'source-map'
   };
